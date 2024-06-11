2d374ba04b0e851764df49827f3da66dabf262c4
Michael Dear <mjdear68@gmail.com>
2024-06-11 19:05:56 +1000

    class_data_prep.ipynb: added No Data class

--- nbk/global_params.py
+++ nbk/global_params.py
@@ -1,16 +1,20 @@
 # Important: Restart the notebook kernel after editing this file or changes will not be imported into the notebook.
 
+# Directories
 netcdf_dir = '../data/netcdf/'
 img_dir = '../data/img/'
 vector_dir = '../data/vector/'
 geotiff_dir = '../data/geoTIFF/'
 climate_dir = '../data/climate/BoM_Nobbys/monthly_temp_rain/'
 
+# Units
 area_unit = 'ha'
 area_unit_val = 10**4
-
 spatial_res = 30 # Spatial resolution of the raster dataset in metres
 crs = 3577
+
+# Value to use for NaN. Required for ML algorithms.
+nodata_val = -999
 
 # Study area abbreviation for exports etc
 study_area_abbrev = 'hunter_estuary'
